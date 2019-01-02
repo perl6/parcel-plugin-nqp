66fdc5dd45c0ba38f9bcce27753214909d1ce9d6
Paweł Murias <pawelmurias@gmail.com>
2019-01-02 18:58:06 +0100

    Register .t for Perl6 files too

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = function(bundler) {
   bundler.addAssetType('nqp', require.resolve('./NQPAsset'));
+  bundler.addAssetType('t', require.resolve('./Perl6Asset'));
   bundler.addAssetType('p6', require.resolve('./Perl6Asset'));
   bundler.addAssetType('nqp-raw-runtime', require.resolve('./NQPRawRuntimeAsset'));
 };
